216b4029aad7ed30a97a5ff64ce3a31fd606c364
lucky99ethan <ethansawe99@gmail.com>
2025-02-03 13:26:29 +0300

    all backend

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const userRoute = require('./routes/user');
 const categoryRoute = require('./routes/category');
 const productRoute = require('./routes/product');
 const billRoute = require('./routes/bill')
+const dashboardRoute = require('./routes/dashboard');
 
 
 
@@ -25,6 +26,7 @@ app.use('/user', userRoute);
 app.use('/category', categoryRoute);
 app.use('/product', productRoute);
 app.use('/bill', billRoute);
+app.use('/dashboard', dashboardRoute)
 
 
 module.exports = app; 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,4 +1,49 @@
   const express = require('express');
   const connection = require('../connection');
   const router = express.Router();
-  
+  var auth = require('../services/authentication');
+
+
+  router.get('/details', auth.authenticateToken,(req,res,next) => {
+    var categoryCount;
+    var productCount;
+    var billcount;
+    var query = "select count(id) as categoryCount from category";
+    connection.query(query, (err, results) => {
+        if(!err){
+            categoryCount = results[0].categoryCount;
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    });
+    var query ="select count(id) as productCount from product";
+    connection.query(query, (err, results) => {
+        if(!err){
+            productCount = results[0].productCount;
+
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+    var query = "select count(id) as billCount from bill";
+    connection.query(query,(err, results) => {
+        if(!err){
+            billcount = results[0].billcount;
+            var data = {
+                category: categoryCount,
+                product: productCount,
+                bill: billcount
+            };
+            return res.status(200).json(data);  
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+ 
+  })
+
+
+  exports = module.exports = router;
